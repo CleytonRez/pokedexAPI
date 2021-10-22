c588939369aa738b337128e73505eab5e1a25654
Cleyton Eduardo Rezende <cleytonmgrezendedev@gmail.com>
2021-10-22 19:07:23 -0300

    feature: Update, Delete, Refactory

--- src/index.js
+++ src/index.js
@@ -1,10 +1,39 @@
 import { readPokemons } from "./services/jsonService";
 import { createPokemon } from "./services/jsonService";
+import { updatePokemon } from "./services/jsonService";
+import { deletePokemon } from "./services/jsonService";
 
 const execute = async () => {
-    const pokemon = await readPokemons();
-    //console.log(pokemon)
+    const pokemonList = await readPokemons();
+    console.log(pokemonList)
+
+    const returnCreate = await createPokemon({
+        "name": "Evee",
+        "strength": 15,
+        "defense": 10,
+        "weaknesses": [
+            "earth"
+        ],
+        "abilities": [
+            {
+                "name": "Attack",
+                "element": "earth",
+                "power": 18
+            }
+        ],
+        "img": null
+    })
+    console.log(returnCreate);
+
+    const returnUpdate = await updatePokemon({
+        id: 1,
+        "strength": 21,
+    });
+    console.log(returnUpdate);
+
+    const returnDelete = await deletePokemon(1);
+    console.log(returnDelete);
 
 }
+
 execute();
-createPokemon({});
--- src/services/jsonService.js
+++ src/services/jsonService.js
@@ -2,66 +2,116 @@ import fs from "fs";
 import util from "util";
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
+const path = "./src/models/pokemon.json"
+
+const _getParseFile = async () => {
+    // Carregar o documento pokemon.json.
+    const file = await readFile(path, "utf8")
+
+    // Fazer um parse para objeto.
+    const parsedJSON = JSON.parse(file)
+
+    return parsedJSON;
+}
+
+const _setStringilyFile = async (data) => {
+    // Fazer o .Stringify do objeto.
+    const stringfiedJSON = JSON.stringify(data);
+    console.log(stringfiedJSON);
+
+    // Subescrever o arquivo pokemon.json.
+    await writeFile(path, stringfiedJSON)
+}
+
 export const readPokemons = async () => {
     try {
         // Carregar o documento pokemon.json.
-        const file = await readFile("./src/models/pokemon.json", "utf8")
-
-        // Fazer um parse para objeto.
-        const pokemons = JSON.parse(file)
-        //console.log(pokemons)
+        const data = await _getParseFile();
 
         // retorna Array pokemon.
-        return pokemons.data;
+        return data.data;
 
     } catch (e) {
-        console.log(e.message)
+        console.log(e)
     }
 }
 
-export const createPokemon = async (pokemon) => {
-    console.log("createPokemon teste")
+export const createPokemon = async (pokemonCreate) => {
+
     // Carregar o documento pokemon.json.
     try {
-        const file = await readFile("./src/models/pokemon.json", "utf8")
-
-        // Fazer um parse para objeto.
-        const pokemons = JSON.parse(file)
+        // Carregar o documento pokemon.json.
+        const data = await _getParseFile();
 
         // Adicionar pokemon na lista.
-        pokemon.id = Math.random()
-        console.log(pokemon)
-        pokemons.data.push(pokemon)
-        console.log(pokemons)
+        pokemonCreate.id = Math.random()
+        data.data.push(pokemonCreate)
 
-        // Fazer o .Stringify do objeto.
-        const pokemonList = JSON.stringify(pokemons);
-        console.log(pokemonList);
 
-        // Subescrever o arquivo pokemon.json.
-        await writeFile("./src/models/pokemon.json", pokemonList)
+        _setStringilyFile(data)
     } catch (e) {
         console.log(e)
 
     }
-
-
-}
-
-const updatePokemon = (pokemon) => {
-    // Carregar o documento pokemon.json.
-    // Fazer um parse para objeto.
-    // buscar o pokemon pelo ID .map quando achar, retorna aquele pokemon com os valores substituidos.
-    // Fazer o .Stringfy do objeto.
-    // Subescrever o arquivo pokemon.json.
+    return pokemonCreate.id;
 
 }
 
-const deletePokemon = (id) => {
+/**
+ * Recebe um OBJ com os novos dados, encontra-lo no JSON e atualizar as informacoes.
+ * @param {newPokemon} pokemonUpdate 
+ */
+export const updatePokemon = async (pokemonUpdate) => {
+
     // Carregar o documento pokemon.json.
-    // Fazer um parse para objeto.
+    try {
+        // Carregar o documento pokemon.json.
+        const data = await _getParseFile();
+
+        // buscar o pokemon pelo ID .map quando achar, retorna aquele pokemon com os valores substituidos.
+        const newPokemonList = data.data.map((pokemon) => {
+            //console.log(obj)
+
+            if (pokemon.id === pokemonUpdate.id) {
+
+                return Object.assign({}, pokemon, pokemonUpdate)
+            }
+            return pokemon;
+        })
+
+        // Fazer o .Stringfy do objeto.
+        _setStringilyFile({
+            data: newPokemonList
+        })
+
+        return pokemonUpdate.id;
+    } catch (e) {
+        console.log(e)
+    }
+
+}
+
+export const deletePokemon = async (id) => {
+
+    // Carregar o documento pokemon.json.
+    const data = await _getParseFile();
+
     // Cria uma newList vazia.
+    const newPokemonList = []
+
     // buscar o pokemon pelo ID .forEach quando achar, cria um if e se for diferente ele adiciona na lista .
+    data.data.forEach((pokemon) => {
+
+        if (pokemon.id !== id) {
+            newPokemonList.push(pokemon)
+        }
+    });
+
     // Fazer o .Stringfy do objeto.
-    // Subescrever o arquivo pokemon.json.
-}
+    _setStringilyFile({
+        data: newPokemonList
+    })
+
+    return id;
+
+};

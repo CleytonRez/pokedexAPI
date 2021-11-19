0a7b0b5f73f534cf4d0e5bcf9f3066b53794d5ef
Cleyton Eduardo Rezende <cleytonmgrezendedev@gmail.com>
2021-11-19 18:40:02 -0300

    feature: Update Files, Rename Keys

--- src/validate/validateRequest.js
+++ src/validate/validateRequest.js
@@ -7,15 +7,15 @@ export const validateId = (id) => {
 }
 export const validateBody = (body, comparator = "AND") => {
     if (comparator === "AND") {
-        if (body.name && body.strength && body.defense && body.weaknesses && body.abilities) {
-            if (body.name.length > 3 && typeof body.strength === "number" && typeof body.defense === "number" && body.weaknesses.length > 0 && body.abilities.length > 0) {
+        if (body.name && body.strength && body.defense && body.weaknessess) {
+            if (body.name.length > 3 && typeof body.strength === "number" && typeof body.defense === "number" && body.weaknesses.length > 0) {
                 return true;
             }
         }
         return false
     };
     if (comparator === "OR") {
-        if (body.name || body.strength || body.defense || body.weaknesses || body.abilities) {
+        if (body.name || body.strength || body.defense || body.weaknesses) {
             return true
         }
     }
